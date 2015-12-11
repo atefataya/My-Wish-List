1633dbdd7df78bfb2b58c217bd5245f930053593
Atef H Ataya <atef.ataya@yahoo.com>
2015-12-11 12:46:15 -0800

    Finishing the add new page wish

---
+++ My-Wish-List/Wish.swift
@@ -0,0 +1,25 @@
+//
+//  Wish.swift
+//  My-Wish-List
+//
+//  Created by Atef H Ataya on 12/11/15.
+//  Copyright © 2015 Atef H Ataya. All rights reserved.
+//
+
+import Foundation
+
+class Post: NSObject, NSCoding {
+    private var _imagePath: String!
+    private var _title: String!
+    private var _wishDesc: String!
+    private var _wishURL: String!
+    private var _wishPrice: String!
+    private var _wishShop: String!
+    private var _wishNotes: String!
+    
+    
+    
+    
+    
+    
+}
