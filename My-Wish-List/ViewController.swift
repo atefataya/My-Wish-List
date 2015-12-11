d81c89b7f2635bb11f46203e94fd5c2c14f40d32
Atef H Ataya <atef.ataya@yahoo.com>
2015-12-11 14:37:15 -0800

    ViewController Done

--- My-Wish-List/ViewController.swift
+++ My-Wish-List/ViewController.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        DataService.instance.loadWishes()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onWishesLoad", name: "wishesLoaded", object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let wish = DataService.instance.loadedWishes[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("WishCell") as? WishCell {
+            cell.configureCell(wish)
+            return cell
+        } else {
+            let cell = WishCell()
+            cell.configureCell(wish)
+            return cell
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 94.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.loadedWishes.count
+    }
+    
+    func onWishesLoad (notif: AnyObject) {
+        tableView.reloadData()
+    }
+  
 
 
 }
